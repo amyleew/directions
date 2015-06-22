008f10bfef2c3856ec46d257747945c80296cdd2
amyleewalton <amylee@mapbox.com>
2015-06-22 17:42:56 -0400

    make retina friendly

--- js/directions.js
+++ js/directions.js
@@ -1,11 +1,11 @@
 /*    Leaflet and the map    */
 
 // initialize the map on the "map" div with a given center and zoom
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([38.9039, -77.0355], 14);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mslee.iyrjm7vi',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> © <a href="http://mapbox.com">Mapbox</a>',
+    detectRetina: true,
+	id: 'mslee.iyrjm7vi',
     accessToken: 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg'
 }).addTo(map);
